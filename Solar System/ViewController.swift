15681f156a68c32c1ce4fded83f2ea7ff7f3dfdc
lylejover <lylejover2yahoo.com>
2018-01-29 22:45:55 -0700

    earth rotating. Added textures

--- Solar System/ViewController.swift
+++ Solar System/ViewController.swift
@@ -17,6 +17,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
         self.sceneView.session.run(configuration)
+        self.sceneView.autoenablesDefaultLighting = true
+    
+ 
         
        
     }
@@ -24,9 +27,23 @@ class ViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         let earth = SCNNode()
         earth.geometry = SCNSphere(radius: 0.2)
-        earth.geometry?.firstMaterial?.diffuse.contents = 
+        earth.geometry?.firstMaterial?.diffuse.contents = #imageLiteral(resourceName: "EarthDay")
+        earth.geometry?.firstMaterial?.specular.contents = #imageLiteral(resourceName: "EarthSpecular")
+        earth.geometry?.firstMaterial?.emission.contents = #imageLiteral(resourceName: "EarthEmission")
+        earth.geometry?.firstMaterial?.normal.contents = #imageLiteral(resourceName: "EarthNormal")
+        earth.position = SCNVector3Make(0, 0, -1)
+        self.sceneView.scene.rootNode.addChildNode(earth)
+        
+        let action = SCNAction.rotateBy(x: 0, y: CGFloat(360.degreesToRadians), z: 0, duration: 8)
+        let forever = SCNAction.repeatForever(action)
+        earth.runAction(forever)
     }
 
 
+}
+
+extension Int {
+    
+    var degreesToRadians: Double { return Double(self) * .pi/180}
 }
 
